74920ee6502af24da9ee7e98a8518ebfca5dc14b
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-17 06:22:08 +0900

    Initial Commit

---
+++ SwiftButton2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftButton2
+//
+//  Created by 野口晃 on 2020/06/17.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
