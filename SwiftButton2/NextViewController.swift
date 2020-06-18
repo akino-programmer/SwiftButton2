917b68ec75fef8e203fdb084f6c3782474c867e4
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-19 05:51:21 +0900

    create NextViewController.

---
+++ SwiftButton2/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  SwiftButton2
+//
+//  Created by 野口晃 on 2020/06/18.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
